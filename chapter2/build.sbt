f8b095fd95ce583905d11e07f6894fca547cd228
Yuriy Stul <ysden123@gmail.com>
2018-11-13 23:28:14 +0200

    refactoring

--- chapter2/build.sbt
+++ chapter2/build.sbt
@@ -2,7 +2,15 @@ organization := "com.stulsoft"
 name := "scala-concurrent-chapter2"
 version := "0.0.1"
 
+lazy val scalaLoggingVersion = "3.9.0"
+lazy val logbackClassicVersion = "1.2.3"
+
 javacOptions ++= Seq("-source", "11")
 
 scalaVersion in ThisBuild := "2.12.7"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.scala-logging" %% "scala-logging" % scalaLoggingVersion,
+  "ch.qos.logback" % "logback-classic" % logbackClassicVersion
+)
 
--- chapter2/src/main/scala/com/stulsoft/concurrent/chapter2/AtomicLock.scala
+++ chapter2/src/main/scala/com/stulsoft/concurrent/chapter2/AtomicLock.scala
@@ -4,31 +4,38 @@
 
 package com.stulsoft.concurrent.chapter2
 
+import com.typesafe.scalalogging.LazyLogging
+
 import scala.concurrent.ExecutionContext
 
 /**
   * @author Yuriy Stul
   */
-object AtomicLock extends App {
+object AtomicLock extends App with LazyLogging {
 
   import java.util.concurrent.atomic._
 
   private val lock = new AtomicBoolean(false)
 
   def mySynchronized(body: => Unit): Unit = {
+    logger.info("==>mySynchronized")
     while (!lock.compareAndSet(false, true)) {}
     try body
     finally lock.set(false)
+    logger.info("<==mySynchronized")
   }
 
   var count = 0
-  for (i <- 0 until 10) execute {
-    mySynchronized {
-      count += 1
+  (0 until 10) foreach { _ =>
+    execute {
+      mySynchronized {
+        count += 1
+      }
     }
   }
+
   Thread.sleep(1000)
-  println(s"Count is: $count")
+  logger.info(s"Count is: $count")
 
   def execute(body: => Unit): Unit = ExecutionContext.global.execute(() => body)
 }
